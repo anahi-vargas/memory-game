abd7c812ca6d6bfe03fde99b6c025c345061f329
alberme <albertmartinez16@gmail.com>
2021-11-23 00:52:19 -0800

    docs(settings): remove comments

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 
 function Settings (props) {
     const [newDifficulty, setNewDifficulty] = useState(props.difficulty);
-    // const [selected, setSelected] = useState(0)
+
     return(
         <div className="settings-container">
             {
@@ -15,9 +15,6 @@ function Settings (props) {
                     </div>
                 )
             }
-        {/* // <div className={`difficulty ${props.difficulty === 0 ? "selected" : ""}`} onClick={() => props.setDifficulty(0)}>Easy</div>
-        // <div className={`difficulty ${props.difficulty === 1 ? "selected" : ""}`} onClick={() => props.setDifficulty(1)}>Medium</div>
-        // <div className={`difficulty ${props.difficulty === 2 ? "selected" : ""}`} onClick={() => props.setDifficulty(2)}>Hard</div> */}
         <button className="settings-button" onClick={() => props.applyChanges(newDifficulty)}>Apply Changes</button>
         <button className="settings-button" onClick={props.setSettings}>Close</button> 
     </div>
